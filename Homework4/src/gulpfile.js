3adbdefeacdf2f6fefea5db3bfba27794d629493
Elena Zapevalova <lenazap97@gamil.com>
2016-11-28 23:11:51 +0600

    homework 6

--- Homework4/src/gulpfile.js
+++ Homework4/src/gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var nunjucks = require('gulp-nunjucks');
 var concat = require('gulp-concat');
+var cssmin = require('gulp-cssmin');
+var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
 
 var path = {
@@ -8,21 +10,41 @@ var path = {
     html: 'templates/*.html',
   	images: 'images/*.*',
 	fonts: 'fonts/*.*',
+ vendor: {
+      css: 'src/vendor/css/*.css'
+   },
  dist: {
       css:  'dist/styles/',
       html: 'dist/',
 	 images: 'dist/images/',
-	 	fonts: 'dist/fonts/'
+	 	fonts: 'dist/fonts/',
+	 vendor: 'dist/vendor/'
  }
+
 };
 
 gulp.task('default', ['build', 'serve', 'watch']);
 
 gulp.task('css', function () {
   return gulp.src(path.css)
+    .pipe(autoprefixer({
+        browsers: ['last 4 versions']
+    }))
     .pipe(concat('style.css'))
     .pipe(gulp.dest(path.dist.css));
+
 });
+
+gulp.task('css-min', function () {
+  return gulp.src(path.css)
+    .pipe(autoprefixer({
+        browsers: ['last 4 versions']
+    }))
+    .pipe(concat('style.css'))
+    .pipe(cssmin())
+    .pipe(gulp.dest(path.dist.css));
+});
+
 
 gulp.task('fonts', function () {
   return gulp.src(path.fonts)
@@ -39,14 +61,27 @@ gulp.task('html', function () {
     .pipe(nunjucks.compile())
     .pipe(gulp.dest(path.dist.html));
 });
+gulp.task('vendor-css', function () {
+  return gulp.src(path.vendor.css)
+    .pipe(concat('vendor.css'))
+    .pipe(gulp.dest(path.dist.vendor));
+});
+
+gulp.task('vendor-css-min', function () {
+  return gulp.src(path.vendor.css)
+    .pipe(concat('vendor.css'))
+    .pipe(cssmin())
+    .pipe(gulp.dest(path.dist.vendor));
+});
 
 gulp.task('build', ['html', 'css', 'images', 'fonts']);
-
+gulp.task('prod', ['html', 'css-min', 'vendor-css-min', 'img']);
 gulp.task('watch', function () {
   	gulp.watch(path.css, ['css']);
   	gulp.watch(path.html, ['html']);
 	gulp.watch(path.html, ['images']);
 	gulp.watch(path.html, ['fonts']);
+ 	gulp.watch(path.vendor.css, ['vendor-css']);
 });
 
 gulp.task('serve', ['watch'], function() {
